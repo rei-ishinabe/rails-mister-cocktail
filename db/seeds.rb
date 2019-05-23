34382cbeb04e9606fd0bae3814e66461460839ae
Rei Ishinabe <rishinabe@gmail.com>
2019-05-23 14:59:03 +0900

    completed the cocktail controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# require 'open-uri'
+# require 'JSON'
+
+# url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+# file = open(url).read
+# json = JSON.parse(file)
+# json.each do |value, key|
+#   Ingredient.create(name: value)
+# end
+
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+Ingredient.create(name: "water")
+Ingredient.create(name: "gin")
+Ingredient.create(name: "whiskey")
+Ingredient.create(name: "ram")
+Ingredient.create(name: "tonic water")
+Ingredient.create(name: "sparkling water")
+Ingredient.create(name: "lime")
