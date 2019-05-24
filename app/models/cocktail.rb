14719d1327ebae80b2b80d02e610b0dd3dd2c0d4
Rei Ishinabe <rishinabe@gmail.com>
2019-05-24 16:14:04 +0900

    added average rating

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -4,4 +4,14 @@ class Cocktail < ApplicationRecord
   has_many :reviews
   validates :name, uniqueness: true, presence: true
   mount_uploader :photo, PhotoUploader
+
+  def average_rating
+    return 'No reviews' if reviews.empty?
+
+    sum = 0.0
+    reviews.each do |review|
+      sum += review[:rating]
+    end
+    return (sum / reviews.count).round
+  end
 end
