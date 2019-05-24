54edfb99623741ea1c19249436da6c0d7af849b6
Rei Ishinabe <rishinabe@gmail.com>
2019-05-24 09:01:45 +0900

    fixed styling

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,34 @@
+class DosesController < ApplicationController
+  def new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new
+    @ingredients = Ingredient.all.order(:name)
+  end
+
+  def create
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new(dose_params)
+    @dose.cocktail = @cocktail
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render new_cocktail_dose_path
+    end
+  end
+
+  def destroy
+    @dose = Dose.find(params[:id])
+    @cocktail = @dose.cocktail
+    if @dose.destroy
+      redirect_to cocktail_path(@cocktail)
+    else
+      render cocktail_path(@cocktail)
+    end
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:ingredient_id, :description, :amount)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  root to: 'cocktails#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :cocktails
+  resources :cocktails, only: [:index, :show, :new, :create] do
+    resources :doses, only: [:new, :create]
+  end
+  resources :doses, only: :destroy
 end
